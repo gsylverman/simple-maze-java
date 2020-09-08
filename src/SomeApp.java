73565e95e43270726b8a8dd51c4f6eb0dea217d6
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-09-08 20:14:03 +0300

    multimazes

--- src/SomeApp.java
+++ src/SomeApp.java
@@ -1,21 +1,40 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class SomeApp {
-    private static Maze maze = new Maze();
+    private static ArrayList<Maze> mazes = new ArrayList<>();
 
     public static void main(String[] args) {
-        int[][] someMaze = {
+        Maze maze1 = new Maze();
+        int[][] someMaze1 = {
                 { 0, 1, 1 ,0, 0, 1, 1 ,0, 1, 1, 0, 1 },
                 { 1, 1, 0 ,1, 0, 2, 0 ,1, 0, 0, 0 ,1 },
                 { 0, 1, 0 ,1, 1, 1, 1 ,1, 1, 1, 1, 1 }
         };
-        maze.setMaze(someMaze);
-        maze.setStart(new Position(0, 11));
+        maze1.setMaze(someMaze1);
+        maze1.setStart(new Position(0, 11));
 
-        if (MazeSolver.solveMaze(maze)) {
-            System.out.println("You won!");
-        } else {
-            System.out.println("No path!");
+        Maze maze2 = new Maze();
+        int[][] someMaze2 = {
+                { 0, 1, 1 ,1, 0, 1, 0 ,0, 1, 1, 0, 1 },
+                { 1, 2, 0 ,1, 0, 1, 1 ,1, 0, 0, 0 ,1 },
+                { 0, 1, 0 ,1, 1, 1, 0 ,1, 1, 1, 1, 1 }
+        };
+        maze2.setMaze(someMaze2);
+        maze2.setStart(new Position(0, 11));
+
+        mazes.add(maze1);
+        mazes.add(maze2);
+        mazeSolver(mazes);
+    }
+    public static void mazeSolver (ArrayList<Maze> mazes) {
+        for (Maze maze : mazes) {
+            if (MazeSolver.solveMaze(maze)) {
+                System.out.println("You won!");
+            } else {
+                System.out.println("No path!");
+            }
+            System.out.println("*********************************");
         }
     }
 
